01e74617f76c3afb4a4e44aaafe2c883e0e3abf2
Emanuel <emanuel@refme.com>
2016-04-13 22:24:17 +0100

    Small refactoring
    Fix indentation.
    
    Use a module instead of a Class. we don't need to create an instance of a class just to run the BitmapEditor.
    
    A method module is a better construct

--- app/bitmap_editor.rb
+++ app/bitmap_editor.rb
@@ -1,36 +1,42 @@
-class BitmapEditor
+# Main Module of the BitmapEditor
+module BitmapEditor
+  # HELP TEXT is constant and freezed in memory for quick access
+  HELP_TEXT <<~EOS
+    ? - Help
+    I M N - Create a new M x N image with all pixels coloured white (O).
+    C - Clears the table, setting all pixels to white (O).
+    L X Y C - Colours the pixel (X,Y) with colour C.
+    V X Y1 Y2 C - Draw a vertical segment of colour C in column X between rows Y1 and Y2 (inclusive).
+    H X1 X2 Y C - Draw a horizontal segment of colour C in row Y between columns X1 and X2 (inclusive).
+    S - Show the contents of the current image
+    X - Terminate the session
+  EOS
 
-  def run
+  def self.run
     @running = true
     puts 'type ? for help'
     while @running
       print '> '
-      input = gets.chomp
-      case input
-        when '?'
-          show_help
-        when 'X'
-          exit_console
-        else
-          puts 'unrecognised command :('
-      end
+      handle_input(gets.chomp)
     end
   end
 
   private
-    def exit_console
-      puts 'goodbye!'
-      @running = false
-    end
 
-    def show_help
-      puts "? - Help
-I M N - Create a new M x N image with all pixels coloured white (O).
-C - Clears the table, setting all pixels to white (O).
-L X Y C - Colours the pixel (X,Y) with colour C.
-V X Y1 Y2 C - Draw a vertical segment of colour C in column X between rows Y1 and Y2 (inclusive).
-H X1 X2 Y C - Draw a horizontal segment of colour C in row Y between columns X1 and X2 (inclusive).
-S - Show the contents of the current image
-X - Terminate the session"
+  def self.handle_input(input)
+    case input
+    when '?'
+      puts HELP_TEXT
+    when 'X'
+      exit_console
+    else
+      puts 'unrecognised command :('
     end
+  end
+
+  def self.exit_console
+    puts 'Goodbye!'
+    @running = false
+  end
+
 end
