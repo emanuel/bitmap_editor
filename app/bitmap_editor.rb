ab80ca4a3a84930a342fc89c714eaecde0293ae2
Emanuel <emanuel@refme.com>
2016-04-15 00:47:32 +0100

    Add Parser Specs

--- app/bitmap_editor.rb
+++ app/bitmap_editor.rb
@@ -1,14 +1,15 @@
-require 'pry'
+# require 'pry'
 # Main Module of the BitmapEditor
 module BitmapEditor
   # constants text messages
+
   HELP_TEXT = <<~EOS
     ? - Help
-    I M N - Create a new M x N image with all pixels coloured white (O).
-    C - Clears the table, setting all pixels to white (O).
-    L X Y C - Colours the pixel (X,Y) with colour C.
-    V X Y1 Y2 C - Draw a vertical segment of colour C in column X between rows Y1 and Y2 (inclusive).
-    H X1 X2 Y C - Draw a horizontal segment of colour C in row Y between columns X1 and X2 (inclusive).
+    I M N - Create a new M x N image with all pixels coloured white (O)
+    C - Clears the table, setting all pixels to white (O)
+    L X Y C - Colours the pixel (X,Y) with colour C
+    V X Y1 Y2 C - Draw a vertical segment of colour C in column X between rows Y1 and Y2 (inclusive)
+    H X1 X2 Y C - Draw a horizontal segment of colour C in row Y between columns X1 and X2 (inclusive)
     S - Show the contents of the current image
     X - Terminate the session
   EOS
@@ -18,6 +19,8 @@ module BitmapEditor
   ERROR_MSG = 'unrecognised command :('
 
   PIXEL_LIMITS = 250
+
+  attr_accessor :image
 
   # responsible for runing the REPL
   module REPL
@@ -49,34 +52,36 @@ module BitmapEditor
   # responsible for parsing the commands
   #
   module Parser
-    # Hash of valid commands known by the parser
-    # with a method name and number of possible params
+    # Hash of valid commands known by the parser with their definition
+    # method name | number of numeric params | number of color params
 
-    COMMAND_LIST = { 'I' => [:create_image,            2],
-                     'C' => [:clear_the_table,         0],
-                     'L' => [:colours_the_pixel,       3],
-                     'V' => [:draw_vertical_segment,   4],
-                     'H' => [:draw_horizontal_segment, 4],
-                     'S' => [:show_contents_image,     0],
-                     '?' => [:show_help,               0],
-                     'X' => [:exit,                    0] }
+    COMMAND_LIST = { 'I' => [:create_image,            2, 0],
+                     'C' => [:clear_table,             0, 0],
+                     'L' => [:colours_pixel,           2, 1],
+                     'V' => [:draw_vertical_segment,   3, 1],
+                     'H' => [:draw_horizontal_segment, 3, 1],
+                     'S' => [:show_contents_image,     0, 0],
+                     '?' => [:show_help,               0, 0],
+                     'X' => [:exit,                    0, 0] }
 
     module_function
 
     def parse(input)
       # if the first char of the input is a know command we process
       # if not the parser returns an error
-      COMMAND_LIST[input[0]] ? process_command!(input) : { ok: false }
+      COMMAND_LIST[input[0]] ? process_command(input) : { ok: false }
     end
 
-    def process_command!(input)
+    def process_command(input)
       params = input.split
       command = params.shift
-      exact_number_of_supported_params = COMMAND_LIST.dig(command, 1)
-
-      return { ok: false } if params.size != exact_number_of_supported_params
-      return { ok: false } if params.any? { |p| p.to_i > PIXEL_LIMITS }
-
+      numeric_params = COMMAND_LIST.dig(command, 1)
+      color_params = COMMAND_LIST.dig(command, 2)
+      total_number_params = numeric_params + color_params
+      return { ok: false } if params.size != total_number_params
+      return { ok: false } if params[0..numeric_params - 1].any? do |p|
+        p.to_i > PIXEL_LIMITS || p.to_i < 1
+      end
       # return the correct method_name and validated params
       { ok: true, command: COMMAND_LIST.dig(command, 0), params: params }
     end
